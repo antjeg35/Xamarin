dc828e61a95ad4b8f3d66f6e1e6df1f7b0651cb3
Antoine JEGOU <ajegou@groupe-beaumanoir.fr>
2020-06-25 09:07:08 +0200

    TP3

--- Module4TP1/Module4TP1/MainPage.xaml.cs
+++ Module4TP1/Module4TP1/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Module4TP1.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace Module4TP1
         public MainPage()
         {
             InitializeComponent();
+            new LoginForm(this.login, this.password, this.isRemind, this.loginForm, this.tweetForm, this.errorLabel, this.connection);
         }
     }
 }
